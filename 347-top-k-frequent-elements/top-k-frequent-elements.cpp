d46980ae37357aa4abbe5617c4a3ea50b02824a3
shobha kumari <shobha.cse23058@rvscollege.ac.in>
2026-09-11 09:25:29 +0530

    Time: 3 ms (51.20%) | Memory: 18 MB (75.80%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> countMap;
-        for (int num : nums) {
-            countMap[num]++;
+         unordered_map<int,int> m;
+        for(int x: nums){
+            m[x]++;
         }
-
-        // Step 2: Create buckets — index = frequency, value = list of numbers
-        vector<vector<int>> buckets(nums.size() + 1);
-        for (auto& entry : countMap) {
-            int num = entry.first;
-            int freq = entry.second;
-            buckets[freq].push_back(num);
+        priority_queue<pair<int,int>> q;
+        for(auto& [nu,fr]:m){
+            q.push({fr,nu});
         }
-
-        // Step 3: Collect top k starting from highest frequency
-        vector<int> result;
-        for (int i = (int)buckets.size() - 1; i >= 0 && (int)result.size() < k; i--) {
-            for (int num : buckets[i]) {
-                result.push_back(num);
-                if ((int)result.size() == k) break;
-            }
+        vector<int> a;
+        while(k--){
+            a.push_back(q.top().second);
+            q.pop();
         }
-
-        return result;
+return a;
     }
 };
-
-
-
-        
